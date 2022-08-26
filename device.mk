9df7c91afc94a2e253192275ac44aac0e5bb89c3
ImRyujinnn <LynzhX0@gmail.com>
2022-08-26 17:50:48 +0700

    agate: stuck logo

--- device.mk
+++ device.mk
@@ -54,7 +54,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl.recovery
+    android.hardware.boot@1.2-impl.recovery
 
 # fastbootd
 PRODUCT_PACKAGES += \
