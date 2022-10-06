9304ec98d9657205f5f9c6cecc49e8fe664c611f
Mikaela <LynzhX0@gmail.com>
2022-10-06 18:56:56 +0700

    agate: update BoardConfig.mk

--- twrp_agate.mk
+++ twrp_agate.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device specific configs
 $(call inherit-product, device/xiaomi/agate/device.mk)
